ff5d4f32af74bedf1ed3512557a09176e1b33b8f
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-09-06 02:18:22 -0400

    Add JugadorRepository.kt

---
+++ RegistroJugadorAp2/app/src/main/java/edu/ucne/RegistroJugadorAp2/domain/repository/JugadorRepository.kt
@@ -0,0 +1,11 @@
+package edu.ucne.RegistroJugadorAp2.domain.repository
+
+import edu.ucne.RegistroJugadorAp2.domain.model.Jugador
+import kotlinx.coroutines.flow.Flow
+
+interface JugadorRepository {
+    fun observeJugadores(): Flow<List<Jugador>>
+    suspend fun getJugador(id: Int): Jugador?
+    suspend fun upsert(jugador: Jugador): Int
+    suspend fun delete(id: Int)
+}

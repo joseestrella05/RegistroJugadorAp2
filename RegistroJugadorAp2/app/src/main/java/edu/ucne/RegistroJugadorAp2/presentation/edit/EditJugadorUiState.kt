af32a9f6a707c2c04c5773584d90ba226a751f0a
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-09-06 02:28:13 -0400

    add EditJugadorUiState

---
+++ RegistroJugadorAp2/app/src/main/java/edu/ucne/RegistroJugadorAp2/presentation/edit/EditJugadorUiState.kt
@@ -0,0 +1,15 @@
+package edu.ucne.RegistroJugadorAp2.presentation.edit
+
+data class EditJugadorUiState(
+    val jugadorId: Int? = null,
+    val nombres: String = "",
+    val partidas: String = "",
+    val nombresError: String? = null,
+    val partidasError: String? = null,
+    val isSaving: Boolean = false,
+    val isDeleting: Boolean = false,
+    val isNew: Boolean = true,
+    val saved: Boolean = false,
+    val deleted: Boolean = false
+)
+

2cadb99a1be8fe143b7990a6f6159260faec0550
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-09-06 02:30:09 -0400

    Add ListJugadorUiEvent

---
+++ RegistroJugadorAp2/app/src/main/java/edu/ucne/RegistroJugadorAp2/presentation/list/ListJugadorUiEvent.kt
@@ -0,0 +1,9 @@
+package edu.ucne.RegistroJugadorAp2.presentation.list
+
+sealed interface ListJugadorUiEvent {
+    data object Load : ListJugadorUiEvent
+    data class Delete(val jugadorId: Int) : ListJugadorUiEvent
+    data object CreateNew : ListJugadorUiEvent
+    data class Edit(val jugadorId: Int) : ListJugadorUiEvent
+    data class ShowMessage(val message: String) : ListJugadorUiEvent
+}

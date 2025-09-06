6b4e338a118f57d8144784d7e4fbdfde34d9ca3f
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-09-06 02:27:38 -0400

    add EditJugadorUiEvent

---
+++ RegistroJugadorAp2/app/src/main/java/edu/ucne/RegistroJugadorAp2/presentation/edit/EditJugadorUiEvent.kt
@@ -0,0 +1,9 @@
+package edu.ucne.RegistroJugadorAp2.presentation.edit
+
+sealed interface EditJugadorUiEvent {
+    data class Load(val id: Int?) : EditJugadorUiEvent
+    data class NombresChanged(val value: String) : EditJugadorUiEvent
+    data class PartidasChanged(val value: String) : EditJugadorUiEvent
+    data object Save : EditJugadorUiEvent
+    data object Delete : EditJugadorUiEvent
+}

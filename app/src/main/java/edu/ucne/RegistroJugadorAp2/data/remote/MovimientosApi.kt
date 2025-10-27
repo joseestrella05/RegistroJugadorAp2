d3e505c7df1f43aa2586cbeb7afb5208420bc204
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-10-27 01:04:56 -0400

    Cambio de ruta del get

--- app/src/main/java/edu/ucne/RegistroJugadorAp2/data/remote/MovimientosApi.kt
+++ app/src/main/java/edu/ucne/RegistroJugadorAp2/data/remote/MovimientosApi.kt
@@ -1,14 +1,10 @@
 package edu.ucne.RegistroJugadorAp2.data.remote
 
-import edu.ucne.RegistroJugadorAp2.domain.model.MovimientoDto
-import retrofit2.http.Body
+import edu.ucne.RegistroJugadorAp2.data.remote.dto.MovimientoDto
 import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface MovimientosApi {
-    @GET("api/Movimientos/1")
-    suspend fun getMovimientos(): List<MovimientoDto>
-
-    @POST("api/Movimientos/1")
-    suspend fun postMovimiento(@Body movimiento: MovimientoDto): MovimientoDto
+    @GET("api/Movimientos/{partidaId}")
+    suspend fun getMovimientos(@Path("partidaId") partidaId: Int): List<MovimientoDto>
 }
